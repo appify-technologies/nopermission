f04219e1b66d7b9107f3eabf18001bd0b87a7081
millwallaby <mill_wallaby@appify-inc.com>
2023-03-19 22:25:14 +0900

    テストにコメントを追加

--- nopermission_test.go
+++ nopermission_test.go
@@ -7,21 +7,25 @@ import (
 	"github.com/gqlgo/gqlanalysis/analysistest"
 )
 
+// Run analysis with no excludes rule.
 func Test(t *testing.T) {
 	testdata := analysistest.TestData(t)
 	analysistest.Run(t, testdata, nopermission.Analyzer(""), "a")
 }
 
+// Run analysis with one excludes rule.
 func TestWithSingleExclusion(t *testing.T) {
 	testdata := analysistest.TestData(t)
 	analysistest.Run(t, testdata, nopermission.Analyzer("ObjectWithoutPermission"), "b")
 }
 
+// Run analysis with two excludes rules.
 func TestWithMultipleExclusions(t *testing.T) {
 	testdata := analysistest.TestData(t)
 	analysistest.Run(t, testdata, nopermission.Analyzer("ObjectWithoutPermission,InterfaceWithoutPermission"), "c")
 }
 
+// Run analysis with one excludes rule in regex.
 func TestWithRegex(t *testing.T) {
 	testdata := analysistest.TestData(t)
 	analysistest.Run(t, testdata, nopermission.Analyzer("Object.+"), "d")
